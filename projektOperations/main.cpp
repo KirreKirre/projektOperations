4b51c501763902a9287bb4c52ecdd2530cdf98a8
KirreKirre <erik_trulsson@hotmail.se>
2018-05-19 13:33:54 +0200

    FirstFit NextFit BestFit started

--- projektOperations/main.cpp
+++ projektOperations/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-
+#include "surgery.h"
+#include "Heap.h"
 //firstFit 
 //dum med mer rum
 //checkar var objektet får plats -->bäst med max heap? 
+
+void firstFit(surgery operations[],HeapType type);
 
 //nextFit 461
 //does it fit were we put our last objket ->yes->put in same bin 
 //=fill until full then take next
 
+void nextFit(surgery operations[],HeapType type);
+
 //bestFit
 //put in tightest spot right now. 
+
+void bestFit(surgery operations[],HeapType type);
 
 int main() {
 
@@ -23,4 +30,16 @@ int main() {
 	/*Dab on them haters*/
 
 	return 0;
+}
+
+void firstFit(surgery operations[], HeapType type)
+{
+}
+
+void nextFit(surgery operations[], HeapType type)
+{
+}
+
+void bestFit(surgery operations[], HeapType type)
+{
 }
